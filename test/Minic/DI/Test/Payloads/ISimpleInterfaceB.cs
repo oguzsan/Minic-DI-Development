44f926dff89387a0ee2172f3211b6d48be8e72c6
Oguz Sandikci <oguzsan@gmail.com>
2018-03-25 11:35:27 +0300

    License.txt file added License header added to all source files

--- test/Minic/DI/Test/Payloads/ISimpleInterfaceB.cs
+++ test/Minic/DI/Test/Payloads/ISimpleInterfaceB.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2018 Oğuz Sandıkçı
+// This code is licensed under MIT license (see LICENSE.txt for details)
+
 using System;
 
 namespace Minic.DI.Test.Payloads
--- test/Minic/DI/Test/Test8_CrossReferences.cs
+++ test/Minic/DI/Test/Test8_CrossReferences.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2018 Oğuz Sandıkçı
+// This code is licensed under MIT license (see LICENSE.txt for details)
+
 using System;
 using Xunit;
 using Minic.DI;
